1b6f8e4a9d4001694ea0b8045db933e9a6db8dd1
Renjiie <you@example.com>
2018-11-03 13:17:45 +0530

    Updates

---
+++ precache-manifest.208801598e5f64bf215d713e202dd736.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/renjith_resume/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8d4d85ec9fa9c0485d84",
+    "url": "/renjith_resume/static/js/runtime~main.8d4d85ec.js"
+  },
+  {
+    "revision": "0f963b4d92438574386d",
+    "url": "/renjith_resume/static/js/main.0f963b4d.chunk.js"
+  },
+  {
+    "revision": "8eba6974333257acc2c4",
+    "url": "/renjith_resume/static/js/1.8eba6974.chunk.js"
+  },
+  {
+    "revision": "0f963b4d92438574386d",
+    "url": "/renjith_resume/static/css/main.99bb310f.chunk.css"
+  },
+  {
+    "revision": "8eba6974333257acc2c4",
+    "url": "/renjith_resume/static/css/1.1abd0bd7.chunk.css"
+  },
+  {
+    "revision": "01fc94a8c6c894d71e8c0d8ab6c01b6f",
+    "url": "/renjith_resume/index.html"
+  }
+];
